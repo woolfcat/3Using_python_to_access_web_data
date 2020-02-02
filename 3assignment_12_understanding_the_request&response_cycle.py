16955841eed4ea42e2a261890184142eb4ea03ee
woolfcat <710086619@qq.com>
2020-02-02 22:45:26 +0800

    Update 3assignment_12_understanding_the_request&response_cycle.py

--- 3assignment_12_understanding_the_request&response_cycle.py
+++ 3assignment_12_understanding_the_request&response_cycle.py
@@ -8,8 +8,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+mysock.send('GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode())
 
 while True:
     data = mysock.recv(512)
